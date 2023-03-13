a74c025aed88a080af1ddd6ad36f445656d5c4b2
ManthanKumar1 <80151387+ManthanKumar1@users.noreply.github.com>
2023-03-13 13:28:20 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express") // framework
+const mongoose = require("mongoose") // library 
+const bodyParser = require('body-parser'); // package (third party middleware)
+const route = require('./routes/route.js');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); //Postman
+
+mongoose.connect("mongodb+srv://satyajitbera1796:18LuJ6umpLy3A35i@cluster0.puyfnq4.mongodb.net/Group65?retryWrites=true&w=majority", {
+    useNewUrlParser: true //Skip
+})
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
