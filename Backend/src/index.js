4e45a7185e84dc855596817b0165d6273c316fb0
ManthanKumar1 <manthantanwar1@gmail.com>
2025-08-04 19:58:25 +0530

    error fix

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,9 +13,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://voluble-mousse-937afe.netlify.app'],
-  credentials: true
-}))
+  origin: [
+    'http://localhost:5173',
+    'https://book-swap-marketplace.netlify.app'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use('/', router)
 
