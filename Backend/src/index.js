b0ce06b840c12ae97adbf2345627a5edcc1254be
ManthanKumar1 <manthantanwar1@gmail.com>
2025-08-04 19:49:02 +0530

    link change

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,33 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const path = require('path')
-const router = require('./routes/route')
+let express = require('express')
+let mongoose = require('mongoose')
+let dotenv = require('dotenv')
+let cors = require('cors')
+let path = require('path')
+let router = require('./routes/route')
 
-const app = express()
+let app = express()
 dotenv.config()
 
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
+  origin: ['http://localhost:5173', 'https://voluble-mousse-937afe.netlify.app'],
+  credentials: true
 }))
 
 app.use('/', router)
 
-app.use(function (req, res) {
-    return res.status(400).send({ status: false, message: "Path Not Found" })
-})
+app.use((req, res) => {
+  return res.status(404).send({ status: false, message: "Path Not Found" })
+});
 
-mongoose.connect(process.env.mongodb)
-    .then(() => console.log("MongoDB is connected successfully"))
-    .catch(err => console.log(err))
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB is connected successfully"))
+  .catch(err => console.error("MongoDB connection error:", err))
 
-app.listen(process.env.PORT, function () {
-    console.log("Express app running on Port " + process.env.PORT)
+let PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Express app running on port ${PORT}`)
 })
